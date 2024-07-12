4b18d51612e2ff5d3ef1e054cd6a00082f66ca38
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-07-12 12:33:10 -0500

    Add page home
    Add html and styles css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component.js'
+import { LabsComponent } from './pages/labs/labs.component.js';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+   {
+      path: 'home',
+      component: HomeComponent
+   },
+   {
+      path: 'labs',
+      component: LabsComponent
+   }
+];
