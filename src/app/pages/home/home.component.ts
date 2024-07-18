bc1fcdaa043e564ab8a9a2d2fe926cd68dd062e7
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-07-17 20:53:47 -0500

    Add function filter states tasks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -23,7 +23,20 @@ export class HomeComponent {
       title: 'Estudiar Platzi',
       completed: false
     }
-  ])
+  ]);
+
+  filter = signal('all');
+  tasksByFilter = computed(() => {
+    const filter = this.filter();
+    const tasks = this.tasks();
+    if (filter === 'pending') {
+      return tasks.filter(task => !task.completed);
+    }
+    if (filter === 'completed') {
+      return tasks.filter(task => task.completed)
+    }
+    return tasks;
+  })
 
   newTaskCtrl = new FormControl('', {
     nonNullable: true,
@@ -99,5 +112,9 @@ export class HomeComponent {
         return task
       })
     })
+  }
+
+  changeFilter(filter: string) {
+    this.filter.set(filter)
   }
 }
